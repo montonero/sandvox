64692aad466e3c72251b7b8f2a3bf0bf0932b97f
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2014-04-22 21:11:54 -0700

    deps: Add stb dependencies

---
+++ deps/stb/update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl http://nothings.org/stb/stb_image_write.h >stb_image_write.h
+curl http://nothings.org/stb/stb_truetype.h >stb_truetype.h
+curl http://nothings.org/stb_image.c >stb_image.h
